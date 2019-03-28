b5981f2d1e66906f4a64051b25f6f74f7f62eb81
Riddle <riddlewoo713@gmail.com>
2019-03-29 01:10:42 +0900

    oh

--- Algorithm and Data Structure/test.py
+++ Algorithm and Data Structure/test.py
@@ -1,15 +1,29 @@
-from itertools import permutations as pm, combinations as cb
+import sys;sys.stdin=open('input.txt','r')
+input=__import__('sys').stdin.readline
 
-arr=[3,6,7,1,5,4]
-arr.sort()
-n=len(arr)
-for i in range(1<<n): #0부터 63까지
-    aa=str(bin(i&63))[2:].count('1')
-    if aa==4:
-        for j in range(n+1):
-            if i&(1<<j):
-                print(arr[j], end=' ')
-        print()
-print()
-for i in cb(arr,4):
-    print(*i)
+change={
+    '1':'0001',
+    '2':'0010',
+    '3':'0011',
+    '4':'0100',
+    '5':'0101',
+    '6':'0110',
+    '7':'0111',
+    '8':'1000',
+    '9':'1001',
+    'A':'1010',
+    'B':'1011',
+    'C':'1100',
+    'D':'1101',
+    'E':'1110',
+    'F':'1111'
+}
+
+for tc in range(int(input())):
+    a,b=input().split()
+    res=''
+    for i in range(int(a)):
+        res+=change[b[i]]
+    print(res)
+    
+    
