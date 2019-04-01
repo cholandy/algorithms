106f27c426c20567baf1c3f7370dea4d406b3c8a
Sangwon Woo <swtm@naver.com>
2019-04-01 19:54:17 +0900

    fuck

--- Algorithm and Data Structure/Baekjoon/1000번대/01463번 1로 만들기/boj1463_cubelover.py
+++ Algorithm and Data Structure/Baekjoon/1000번대/01463번 1로 만들기/boj1463_cubelover.py
@@ -1,9 +1,9 @@
-for tc in range(int(input())):
-    n,m=map(int,input().split())
-    d=[0 for i in range(m+1)]
-    for i in range(2,n+1):
-        d[i]=min(d[i-1]+1,d[i-1]+10,d[i-1]-1)
-        if i%2==0:
-            d[i]=min(d[i],d[i//2]+1)
-    print(d[m]-d[n])
-    
+n=int(input())
+d=[0 for i in range(n+1)]
+for i in range(2,n+1):
+    d[i]=d[i-1]+1
+    if i%2==0:
+        d[i]=min(d[i],d[i//2]+1)
+    if i%3==0:
+        d[i]=min(d[i],d[i//3]+1)
+print(d[n])
--- Algorithm and Data Structure/test.py
+++ Algorithm and Data Structure/test.py
@@ -1,9 +1,37 @@
-# import sys;sys.stdin=open('input1.txt','r')
-a=set([2,3])
-b=set([3,4])
-c=set([1,5])
-print(a)
-if a|c:
-    a=a|c
-    print(a)
-else:print("no")
+import sys;sys.stdin=open('input1.txt','r')
+from collections import deque
+dx=[1,-1,10]
+
+for tc in range(int(input())):
+    n,m=map(int,input().split())
+    visited=[0]*10000000
+    cnt=0
+    Q=deque((m,cnt))
+    visited[m]=1
+    while Q:
+        a,b=Q.popleft()
+        if a==n:
+            print(b)
+            break
+        if 1<=a<=1000000:
+            for i in range(4):
+                if i==0:
+                    if a%2==0:
+                        new_a=a//2
+                        if visited[new_a]==1:continue
+                        Q.append((new_a,b+1))
+                else:
+                    new_a=a+dx[i-1]
+                    if visited[new_a]==1:continue
+                    Q.append((new_a,b+1))
+    
+        
+
+
+    
+    
+    
+
+
+    
+    
