20016a7b94d3a8d57d9c2aac03c35456eb14421a
Riddle <riddlewoo713@gmail.com>
2019-04-04 00:33:48 +0900

    finish pinball game

--- Algorithm and Data Structure/SW Expert Academy/Solving Club Problem Box/702호/31일차/5650번 핀볼 게임/swea5650_난나눈누.py
+++ Algorithm and Data Structure/SW Expert Academy/Solving Club Problem Box/702호/31일차/5650번 핀볼 게임/swea5650_난나눈누.py
@@ -1,1 +1,40 @@
 import sys;sys.stdin=open('input1.txt','r')
+ewsn=[[0,0,1],[1,0,-1],[2,1,0],[3,-1,0]]
+direction=[0,[1,3,0,2],[1,2,3,0],[2,0,3,1],[3,0,1,2],[1,0,3,2]]
+
+for tc in range(int(input())):
+    N=int(input())
+    mat=[]
+    warmhole=[[] for _ in range(11)]
+    for row in range(N+2):
+        if row==0 or row==N+1:mat.append([5]*(N+2));continue
+        tmp=list(map(int,input().split()))
+        tmp.insert(0,5)
+        tmp.append(5)
+        for col in range(len(tmp)):
+            if tmp[col]>5:
+                warmhole[tmp[col]].append([row,col])
+      
+        mat.append(tmp)
+    ans=0
+    for i in range(1,N+1):
+        for j in range(1,N+1):
+            sx,sy=i,j
+            if mat[i][j]:continue
+            for k in range(4):
+                cnt=0
+                dn,dx,dy=ewsn[k]
+                nx,ny=i+dx,j+dy
+                while 1:
+                    if [nx,ny]==[sx,sy] or mat[nx][ny]==-1:
+                        if ans<cnt:ans=cnt;break
+                        break
+                    if 0<mat[nx][ny]<6:
+                        dn,dx,dy=ewsn[direction[mat[nx][ny]][dn]]
+                        cnt+=1
+                    elif 5<mat[nx][ny]<11:
+                        if [nx,ny]==warmhole[mat[nx][ny]][0]:
+                            nx,ny=warmhole[mat[nx][ny]][1]
+                        else:nx,ny=warmhole[mat[nx][ny]][0]
+                    nx,ny=nx+dx,ny+dy
+    print("#{} {}".format(tc+1,ans))
--- Algorithm and Data Structure/test2.py
+++ Algorithm and Data Structure/test2.py
@@ -1,2 +1,5 @@
 import sys;sys.stdin=open('input2.txt','r')
 
+[nx,ny]=[1,1]
+[sx,sy]=[1,1]
+print([nx,ny]==[sy,sy])
