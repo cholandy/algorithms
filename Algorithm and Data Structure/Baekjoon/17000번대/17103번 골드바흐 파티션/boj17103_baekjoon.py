d8a3595afa8060067454b65a7aa1273ec3723ace
Riddle <riddlewoo713@gmail.com>
2019-05-12 16:50:33 +0900

    boj17103 complete

---
+++ Algorithm and Data Structure/Baekjoon/17000번대/17103번 골드바흐 파티션/boj17103_baekjoon.py
@@ -0,0 +1,21 @@
+import sys;sys.stdin=open('input.txt.','r')
+
+from collections import deque
+primes = deque()
+chck=[False]*1000001
+for i in range(2, 1000001):
+    if chck[i]==False:
+        primes.append(i)
+        for j in range(2*i,1000001,i):
+            chck[j]=True
+
+
+for tc in range(int(input())):
+    n = int(input())
+    ans=0
+    for p in primes:
+        if n-p>=2 and p<=n-p:
+            if chck[n-p]==False:
+                ans+=1
+        else:break
+    print(ans)
