7a613490545e1543377d418fc3c8e041ee864764
Riddle Woo <riddlewoo713@gmail.com>
2019-07-24 11:05:40 +0900

    All files ready

--- Codeground Src/test.cpp
+++ Codeground Src/test.cpp
@@ -2,10 +2,10 @@
 
 int main()
 {
-    int a(101);
-    int b(102);
-    float c = (float)(a+b)/2;
-    printf("%f\n", c);
+    int a(11);
+    int b(12);
+    int c = (a+b)/3;
+    printf("%d\n", c);
 
     return 0;
 }
