67de2d32ed5a2eabecfc1ec31f374a6453584ffa
Sangwon Woo <swtm@naver.com>
2019-10-01 17:51:14 +0900

    To Do: BOJ3088

--- Algorithm and Data Structure/Baekjoon/3000번대/03088번 화분 부수기/boj3088_firestone.py
+++ Algorithm and Data Structure/Baekjoon/3000번대/03088번 화분 부수기/boj3088_firestone.py
@@ -6,3 +6,17 @@ for _ in range(N):
     arr.append((a, b, c))
 
 ans = 0
+ret = []
+for points1 in range(int(N/2)):
+    key = True
+    count = 0
+    for points2 in arr[points1+1:]:
+        if set(arr[points1], arr[points2]) != None:
+            count += 1
+            continue
+        else:
+            key = False
+        ret.append((points1, count))
+
+        if key == False:
+            continue
