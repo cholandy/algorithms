33d8a8447f1721d41fd0bbb70c3c3bd8d2168cbb
Sangwon Woo <swtm@naver.com>
2019-10-01 18:00:01 +0900

    To Do: BOJ3088

--- Algorithm and Data Structure/Baekjoon/3000번대/03088번 화분 부수기/boj3088_firestone.py
+++ Algorithm and Data Structure/Baekjoon/3000번대/03088번 화분 부수기/boj3088_firestone.py
@@ -1,22 +1,22 @@
 from collections import deque
 N = int(input())
 arr = deque()
+check = []
+
+
+def poss(a, b):
+    return set(a, b)
+
+
 for _ in range(N):
-    a, b, c = map(int, input().split())
-    arr.append((a, b, c))
-
-ans = 0
-ret = []
-for points1 in range(int(N/2)):
-    key = True
-    count = 0
-    for points2 in arr[points1+1:]:
-        if set(arr[points1], arr[points2]) != None:
-            count += 1
-            continue
-        else:
-            key = False
-        ret.append((points1, count))
-
-        if key == False:
-            continue
+    if _ == 0:
+        check = map(int, input().split())
+        continue
+    ans = 0
+    comp = []
+    comp = map(int, input().split())
+    if poss(check, comp) != None:
+        check = comp
+        continue
+    else:
+        arr.append(comp)
