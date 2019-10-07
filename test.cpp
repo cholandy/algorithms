08bd72227db6f93630ddbe9259b02e1180874091
Riddle <riddlewoo713@gmail.com>
2019-10-07 23:15:33 +0900

    Update: Test.cpp

--- test.cpp
+++ test.cpp
@@ -1,27 +1,15 @@
+#pragma warning(disable : 4996)
 #include <bits/stdc++.h>
 
 using namespace std;
 
-void func(vector<int> &v)
-{
-    for (int i = 0; i < v.size(); ++i)
-    {
-        v[i] = v[i] % 2;
-    }
-}
-
 int main()
 {
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-    for (int i = 0; i < v.size(); ++i)
+    int n = 5;
+    int arr[n] = {1, 2, 3};
+
+    for (int i = 0; i < 5; ++i)
     {
-        cout << v[i] << ' ';
+        printf("%d ", arr[i]);
     }
-    cout << '\n';
-    func(v);
-    for (int i = 0; i < v.size(); ++i)
-    {
-        cout << v[i] << ' ';
-    }
-    return 0;
 }
