7accdf552c368526372e6738f2fbb7e9d46a12de
Sangwon Woo <swtm@naver.com>
2019-10-08 14:53:08 +0900

    New: BOJ2884

---
+++ Algorithm and Data Structure/Baekjoon/2000번대/02800번대/02884번 알람 시계/boj2884_firestone.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+using namespace std;
+
+int h, m;
+
+int main() {
+    scanf("%d %d", &h, &m);
+    if (m < 45) {
+        if (h==0) {
+            printf("%d %d", 23, m+15);
+        } else {
+            printf("%d %d", h-1, m+15);
+        }
+    } else {
+        printf("%d %d", h, m-45);
+    }
+
+    return 0;
+}
