c1c4291eefb99614bec2afc59cefbd4ca756683e
Riddle <riddlewoo713@gmail.com>
2019-10-09 01:39:44 +0900

    Update: test.cpp

--- test.cpp
+++ test.cpp
@@ -5,11 +5,17 @@ using namespace std;
 
 int main()
 {
-    int n = 5;
-    int arr[n] = {1, 2, 3};
-
-    for (int i = 0; i < 5; ++i)
-    {
-        printf("%d ", arr[i]);
-    }
+    vector<int> v1 = {1, 2, 3, 4, 5};
+    for (vector<int>::iterator it = v1.begin(); it != v1.end(); it++)
+        cout << *it << ' ';
+    printf("\n");
+    for (auto elem : v1)
+        cout << elem << ' ';
+    cout << '\n';
+    set<int> s1 = {2, 2, 3, 4, 5};
+    for (set<int>::iterator it = s1.begin(); it != s1.end(); it++)
+        cout << *it << ' ';
+    cout << '\n';
+    for (auto elem : s1)
+        cout << elem << ' ';
 }
