e59b3f75cf6c6eb5d7a9d233640827bb4323371a
Sangwon Woo <swtm@naver.com>
2019-10-10 14:56:00 +0900

    Update: input.txt

--- Algorithm and Data Structure/SW Expert Academy/Problems/02001번 파리 퇴치/swea2001_firestone.py
+++ Algorithm and Data Structure/SW Expert Academy/Problems/02001번 파리 퇴치/swea2001_firestone.py
@@ -1,1 +1,12 @@
+import sys
+sys.stdin = open('input.txt', 'r')
 for _ in range(int(input())):
+    N, M = map(int, input().split())
+    grid = [list(map(int, input().split())) for _ in range(N)]
+    dp = [[0] * (N-1) for _ in range(N-1)]
+    # [print(*i) for i in dp]
+    ans = 0
+    for i in range(1, N):
+        for j in range(1, N):
+            dp[i][j] = sum(grid[x][y] for x in range())
+            ans = max(ans, dp[i][j])
