cd5444a649b0254233105a8b5878beab3214649e
Sangwon Woo <swtm@naver.com>
2019-10-10 15:09:05 +0900

    Complete: SWEA2001

--- Algorithm and Data Structure/SW Expert Academy/Problems/02001번 파리 퇴치/swea2001_firestone.py
+++ Algorithm and Data Structure/SW Expert Academy/Problems/02001번 파리 퇴치/swea2001_firestone.py
@@ -6,7 +6,9 @@ for _ in range(int(input())):
     dp = [[0] * (N-1) for _ in range(N-1)]
     # [print(*i) for i in dp]
     ans = 0
-    for i in range(1, N):
-        for j in range(1, N):
-            dp[i][j] = sum(grid[x][y] for x in range())
-            ans = max(ans, dp[i][j])
+    for i in range(M-1, N):
+        for j in range(M-1, N):
+            dp[i-1][j-1] = sum(grid[x][y] for x in range(i+1-M, i+1)
+                               for y in range(j+1-M, j+1))
+            ans = max(ans, dp[i-1][j-1])
+    print("#{} {}".format(_+1, ans))
