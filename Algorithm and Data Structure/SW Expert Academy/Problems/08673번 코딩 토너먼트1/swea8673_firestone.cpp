10246a1fe959843144d3aa376f08e36f4d7b7c73
Riddle <riddlewoo713@gmail.com>
2019-10-10 23:07:14 +0900

    Update

--- Algorithm and Data Structure/SW Expert Academy/Problems/08673번 코딩 토너먼트1/swea8673_firestone.cpp
+++ Algorithm and Data Structure/SW Expert Academy/Problems/08673번 코딩 토너먼트1/swea8673_firestone.cpp
@@ -3,22 +3,47 @@
 
 using namespace std;
 
+int abs(int a, int b) {
+    if (a>=b) return a-b;
+    return b-a;
+}
+int max(int a, int b) {
+    if (a>=b) return a;
+    return b;
+}
+
 int main(int argc, char** argv)
 {
 	int test_case;
 	int T;
-	//freopen(&quot;input.txt&quot;, &quot;r&quot;, stdin);
+	freopen("C:\\Users\\woosa\\Desktop\\algorithms\\input.txt", "r", stdin);
 	cin>>T;
 	for(test_case = 1; test_case <= T; ++test_case)
 	{
-        int K;
+        int K, ans=0;
         cin >> K;
-        const int a = 2<<K;
+        const int a = 1<<K;
+        int cnt = 1<<(1-K);
         int arr[a] = {1,};
         for (int i=0; i<a; i++) {
             cin >> arr[i];
         }
-        for 
+        while (cnt >=1) {
+            int next = cnt>>1;
+            int temp[next]={0,};
+            for (int i=0; i<cnt; i++) {
+                ans += abs(arr[2*i], arr[2*i+1]);
+                if (i%2==0) {
+                    temp[i-1] = max(arr[2*i], arr[2*i+1]);
+                }
+            }
+            int arr[next];
+            for (int i=0; i<next; i++) {
+                arr[i] = temp[i];
+            }
+            cnt /=2;
+        }
+        printf("#%d %d\n", test_case, ans);
 
 	}
 	return 0;//정상종료시 반드시 0을 리턴해야합니다.
