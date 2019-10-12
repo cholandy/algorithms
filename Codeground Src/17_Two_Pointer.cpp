926f55a9da91dc5f5b18f97282c1720ce9f00ac5
Riddle <riddlewoo713@gmail.com>
2019-10-12 19:23:09 +0900

    fuck

--- Codeground Src/17_Two_Pointer.cpp
+++ Codeground Src/17_Two_Pointer.cpp
@@ -1,26 +1,27 @@
 #include <stdio.h>
-
+#include <bits/stdc++.h>
 #define MAX 5005
 
 int A[MAX], B[MAX];
 int Sorted[MAX + MAX];
+vector<int> odo;
 
-int main() {
+int main()
+{
 	int n, m;
-
-	scanf("%d", &n);
 
 	for (int i = 0; i < n; i++)
 		scanf("%d", &A[i]);
 
 	scanf("%d", &m);
 
-	for(int i = 0 ; i < m ; i++)
+	for (int i = 0; i < m; i++)
 		scanf("%d", &B[i]);
 
 	int a = 0, b = 0;
 
-	for (int i = 0; i < n + m; i++) {
+	for (int i = 0; i < n + m; i++)
+	{
 		if (b == m || (b != m && a < n && A[a] < B[b]))
 			Sorted[i] = A[a++];
 		else
