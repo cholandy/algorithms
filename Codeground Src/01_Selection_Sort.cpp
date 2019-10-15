c4aea0f83a1414bf99df7834a983429de1946274
Sangwon Woo <swtm@naver.com>
2019-10-15 10:13:23 +0900

    DONE: TEST

--- Codeground Src/01_Selection_Sort.cpp
+++ Codeground Src/01_Selection_Sort.cpp
@@ -5,31 +5,39 @@
 int arr[MAX];
 int N;
 
-void Swap(int *a, int *b) {
+void Swap(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void selection_sort(int *arr, int size) {
-    for (int i = 0; i < size; i++) {
+void selection_sort(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
         int minidx = i;
-        for (int j = i + 1; j < size; j++) {
-            if (arr[minidx] > arr[j]) {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[minidx] > arr[j])
+            {
                 minidx = j;
             }
         }
         Swap(&arr[minidx], &arr[i]);
     }
 }
-int main() {
-    freopen("input.txt", "r", stdin);
-    scanf("%d",&N);
-    for(int i=0; i<N; i++) {
-        scanf("%d",&arr[i]);
+int main()
+{
+    freopen("C:\\Users\\multicampus\\Desktop\\algorithms\\input.txt", "r", stdin);
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
     }
     selection_sort(arr, N);
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N; i++)
+    {
         printf("%d ", arr[i]);
     }
     return 0;
