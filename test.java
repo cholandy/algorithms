142d7ae851f4ada085a6a2bb747597c5cd5af5c6
Sangwon Woo <swtm@naver.com>
2019-10-18 11:22:50 +0900

    Update: test.java'

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 10;
+        for (int i = a; i > 0; --i) {
+            System.out.println(i);
+        }
+    }
+
+}
