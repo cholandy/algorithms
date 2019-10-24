50252c6e9381810acc6d8388007dc093b63b8ac2
Riddle <riddlewoo713@gmail.com>
2019-10-24 23:30:16 +0900

    C++ Programming Page 49

---
+++ 정보올림피아드/c++프로그래밍/p45_파일의_끝_EOF_까지입력.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    while (cin >> a)
+    {
+        break;
+    }
+    return 0;
+}
---
+++ 정보올림피아드/c++프로그래밍/p48_파일로_정수_2개_입력.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    ifstream in("input.txt");
+
+    in >> a >> b;
+
+    return 0;
+}
