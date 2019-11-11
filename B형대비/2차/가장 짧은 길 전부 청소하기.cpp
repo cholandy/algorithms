4243bbed58e49a909be2993b88185ef037121e3d
Sangwon Woo <swtm@naver.com>
2019-11-11 19:58:58 +0900

    Update: test.cpp

---
+++ B형대비/2차/가장 짧은 길 전부 청소하기.cpp
@@ -0,0 +1,4 @@
+#include <stdio.h>
+// G = (V, E) 그래프에서, 어떤 지점 S로부터 출발하여 다른 모든 정점으로 가는 최단 경로들을 나타낸 그래프를 shortest path DAG라 한다.
+// DAG란 Directed Acyclic Graph의 약자로, 방향성이 있고, 사이클이 존재하지 않는 그래프이다. 
+// 필요한 알고리즘: 다익스트라
